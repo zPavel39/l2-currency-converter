a43d9d3da369809e3785086236f0e1941ad6b266
zPavel39 <golubev33108ai@gmail.com>
2024-03-13 21:54:23 +0200

    Fix: style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,9 @@ import ReloadPrompt from '../components/update-project-sw/ReloadPrompt'
 
 function App() {
 	return (
-		<div className='h-screen bg-main-map bg-cover bg-center flex-col justify-center'>
+		<div className='h-dvh bg-main-map bg-cover bg-center flex-col justify-center'>
 			<ReloadPrompt />
-			<div className='absolute inset-0 bg-black opacity-60 z-2'></div>
+			<div className='absolute h-dvh inset-0 bg-black opacity-60 z-2'></div>
 			<Header />
 			<div className='container mx-lg relative  z-10'>
 				<blockquote className='flex flex-col justify-end items-end gap-4 mb-10 mx-5'>
