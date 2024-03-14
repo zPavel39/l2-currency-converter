98c48a7727d1f4020aedba5597c2dc0b380dc3d6
zPavel39 <golubev33108ai@gmail.com>
2024-03-14 23:51:02 +0200

    add: react-select, customSelect, customInput

--- src/components/main-form/MainForm.tsx
+++ src/components/main-form/MainForm.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
+import Select from 'react-select'
 
+import CustomInput from '../ui/input/CustomInput'
+const options = [
+	{ value: 'Аденка(кк)', label: 'Аденка(кк)' },
+	{ value: 'Рубли', label: 'Рубли' },
+	{ value: 'USDT', label: 'USDT' },
+]
 const MainForm = () => {
 	return (
 		<div className=' relative flex flex-col gap-4 mx-4 p-9 max-w-450px'>
 			<div className='blurred-background absolute inset-0 z-10 '></div>
 			<div className='flex flex-col gap-4 z-20'>
-				<input
-					type='number'
-					placeholder='Введите кол-во валюты'
-					className='w-72 p-2 text-xl rounded'
-				/>
-				<input
-					type='number'
-					placeholder='Введите кол-во валюты'
-					className='w-72 p-2 text-xl rounded'
-				/>
+				<div className='flex items-end'>
+					<CustomInput name={'Aденка'} />
+					<Select
+						classNamePrefix='select-style'
+						placeholder={`Валюта`}
+						options={options}
+					/>
+				</div>
+				<div className='flex items-end'>
+					<CustomInput name={'Рубли'} />
+					<Select
+						classNamePrefix='select-style'
+						placeholder={`Валюта`}
+						options={options}
+					/>
+				</div>
 			</div>
 		</div>
 	)
---
+++ src/components/ui/input/CustomInput.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CustomInput = ({ ...props }: any) => {
+	return (
+		<div className='flex items-start flex-col '>
+			<label className='text-white'>{props.name}</label>
+			<input
+				type='number'
+				placeholder='Введите кол-во...'
+				className='w-72 p-2 text-xl rounded rounded-r-none'
+			/>
+		</div>
+	)
+}
+
+export default React.memo(CustomInput)
