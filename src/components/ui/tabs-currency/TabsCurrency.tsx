8bf4e0a31c4d1eb62bc0c8ad513e990ba924e389
zPavel39 <golubev33108ai@gmail.com>
2024-04-30 11:13:08 +0200

    Fix: handleСhange

--- src/components/ui/tabs-currency/TabsCurrency.tsx
+++ src/components/ui/tabs-currency/TabsCurrency.tsx
@@ -14,7 +14,7 @@ function a11yProps(index: number) {
 export default function VerticalTabs() {
 	const [value, setValue] = React.useState(0)
 
-	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+	const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
 		setValue(newValue)
 	}
 
